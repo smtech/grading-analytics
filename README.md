bdc4d2c2e3ea4a983f964d29600304cdfde2ede5
Seth Battis <SethBattis@stmarksschool.org>
2014-07-16 13:45:29 -0400

    Isolate the Calendar↔ICS tool as its own tool.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-canvas-ics-sync
+Canvas ICS Sync
 ===============
 
-Synchronize ICS and webcal feeds into Canvas (per course, user or group)
+A pair (trio?) of tools for working with Canvas and ICS feeds. There is an export tool that exposes the pre-existing ICS feed for course calendars and there is an import tool that pairs an ICS feed with (theoretically) a course, group or user in Canvas and imports all of the ICS events into that calendar, deleting any residual events created by prior imports of that pairing. The quasi-third tool, a sync tool, is really just a wrapper for using crontab to trigger regular re-imports of an ICS feed pairing.
+
+Some care has been taken to protect privacy by not caching the actual calendar events in our MySQL database cache of ICS/Canvas pairings, but, of course, potentially private information is passing through third party hands, etc., etc.
+
+This would benefit from an OAuth setup, so that individual users could set up their own pairings. However, at the moment, it requires administrative intervention and relies on a single API user, Calendar API Process, to handle all imports. The API user is an admin on our main account.
+
---
+++ config.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+/* this will break if there are mod_rewrites, but will do for now... */
+define('APP_URL', 'http://' . $_SERVER['SERVER_NAME'] . str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath(__DIR__)));
+define('APP_PATH', realpath(__DIR__));
+
+/* customize generated pages */
+define('SCHOOL_NAME', 'St. Mark&rsquo;s School');
+define('SCHOOL_URL', 'http://www.stmarksschool.org');
+define('SCHOOL_CANVAS_INSTANCE', 'https://stmarksschool.instructure.com');
+define('SCHOOL_DEPT', 'Academic Technology');
+define('SCHOOL_DEPT_URL', 'http://area51.stmarksschool.org');
+define('SCHOOL_ADDRESS', '25 Marlboro Road, Southborough, Massachusetts, 01772');
+define('SCHOOL_COLOR_LIGHT', 'white'); // masthead foreground
+define('SCHOOL_COLOR_DARK', '#003359'); // masthead background and link colors
+
+define('TOOL_NAME', 'Canvas Calendar &harr; ICS Tool');
+define('TOOL_START_PAGE', dirname($_SERVER['PHP_SELF']));
+
+define('LOCAL_TIMEZONE', 'US/Eastern'); // TODO: Can we detect the timezone for the Canvas instance and use it?
+define('SEPARATOR', '_'); // used when concatenating information in the cache database
+define('SYNC_TIMESTAMP_FORMAT', 'Y-m-d\TH:iP'); // same as CANVAS_TIMESTAMP_FORMAT, FWIW
+
+define('WORKING_DIR', '/var/www-data/canvas/calendar-ics/');
+
+define('API_CLIENT_ERROR_RETRIES', 1);
+define('API_SERVER_ERROR_RETRIES', 3);
+
+?>
