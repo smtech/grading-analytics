0acccb9095a31fed4120be990a767b65b62e55b2
Seth Battis <SethBattis@stmarksschool.org>
2014-08-27 22:47:01 -0400

    Purge
    Rolling out a calendar event purging script.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

---
+++ purge.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once(__DIR__ . '/.ignore.calendar-ics-authentication.inc.php');
+require_once(__DIR__ . '/config.inc.php');
+
+require_once(APP_PATH . '/include/page-generator.inc.php');
+require_once(APP_PATH . '/include/canvas-api.inc.php');
+require_once(APP_PATH . '/include/mysql.inc.php');
+
+$eventsApi = new CanvasApiProcess(CANVAS_API_URL, CANVAS_API_TOKEN);
+$api = new CanvasApiProcess(CANVAS_API_URL, CANVAS_API_TOKEN);
+
+// TODO work nicely with the cache (purge uncached events, or only cached events, etc.)
+
+$events = $eventsApi->get('calendar_events'
+	array(
+		'type' => 'event',
+		'all_events' => true,
+		'context_codes[]' => preg_replace('|.*/courses/(\d+)/?.*|', "course_$1", $_REQUEST['course_url'])
+	)
+);
+print_r($events);
+exit;
+do {
+	foreach($events as $event) {
+		$api->delete("calendar_events/{$event['id']}",
+			array(
+				'cancel_reason' => TOOL_NAME . " course_url={$_REQUEST['course_url']}"
+			)
+		);
+	}
+} while($events = $eventsApi->nextPage())
+
+?>
