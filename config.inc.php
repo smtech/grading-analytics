02ae0a107d4a4d4297875f42c4a08363075fa102
Seth Battis <sethbattis@stmarksschool.org>
2014-11-27 14:38:13 -0500

    Inelegant
    Addressing [this issue](https://github.com/smtech/smcanvas-grading-analytics/issues/5) -- right now it's hard-coded, but there should really be a central preferences database for this kind of information (with a GUI, even…), rather than these stupid config.inc.php files.

--- config.inc.php
+++ config.inc.php
@@ -29,4 +29,7 @@ define('GRAPH_INSET_WIDTH', '40%'); // width of the inset departmental graphs in
 
 require_once(__DIR__ . '/smcanvaslib/config.inc.php');
 
+if (!defined('APP_URL')) {
+	define('APP_URL',"http://skunkworks.stmarksschool.org/canvas/javascript/grading-analytics");
+}
 ?>
