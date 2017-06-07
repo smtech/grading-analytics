b5fecdd05a61f95b00a838ec9430ea28cbc252a9
Seth Battis <sethbattis@stmarksschool.org>
2017-06-06 21:23:02 -0400

    Improve responsiveness
    Let's not make a gazillion queries if we can just cache one and be done with it.

--- course/index.php
+++ course/index.php
@@ -3,25 +3,15 @@
 require_once 'common.inc.php';
 
 use Battis\DataUtilities;
-
-$statistics = $toolbox->mysql_query("
-	SELECT * FROM `course_statistics`
-		WHERE
-			`course[id]` = '" . $_SESSION[COURSE_ID] . "'
-		ORDER BY
-			`timestamp` DESC
-		LIMIT 1
-");
-$statistic = $statistics->fetch_assoc();
+use smtech\GradingAnalytics\Toolbox;
 
 $toolbox->getSmarty()->addStylesheet(DataUtilities::URLfromPath(__DIR__ . '/css/index.css.php'));
 $toolbox->smarty_assign([
-    'statistic' => $statistic,
-    'averageTurnAround' => $toolbox->averageTurnAround(),
-    'averageTurnAroundDepartment' => $toolbox->averageTurnAround($statistic['course[account_id]']),
-    'averageTurnAroundDepartment' => $toolbox->averageTurnAround($statistic['course[account_id]']),
-    'averageAssignmentCount' => $toolbox->averageAssignmentCount(),
-    'averageAssignmentCountDepartment' => $toolbox->averageTurnAround($statistic['course[account_id]'])
+    'statistic' => $toolbox->getCourseSnapshot($_SESSION[COURSE_ID]),
+    'averageTurnAround' => $toolbox->averageTurnAround($_SESSION[COURSE_ID], Toolbox::SCHOOL),
+    'averageTurnAroundDepartment' => $toolbox->averageTurnAround($_SESSION[COURSE_ID], Toolbox::DEPT),
+    'averageAssignmentCount' => $toolbox->averageAssignmentCount($_SESSION[COURSE_ID], Toolbox::SCHOOL),
+    'averageAssignmentCountDepartment' => $toolbox->averageAssignmentCount($_SESSION[COURSE_ID], Toolbox::DEPT)
 ]);
 $toolbox->getSmarty()->addScript(
     DataUtilities::URLfromPath(__DIR__ . '/../vendor/npm-asset/chart.js/dist/Chart.min.js')
--- src/Toolbox.php
+++ src/Toolbox.php
@@ -6,6 +6,18 @@ use smtech\LTI\Configuration\Option;
 
 class Toolbox extends \smtech\StMarksReflexiveCanvasLTI\Toolbox
 {
+    protected $courseHistory = [];
+
+    const DEPT = 0;
+    const SCHOOL = 1;
+
+    protected $snapshots = [[], []];
+
+    const AVERAGE_TURN_AROUND = 0;
+    const AVERAGE_ASSIGNMENT_COUNT = 1;
+
+    protected $numbers = [];
+
     public function getGenerator()
     {
         parent::getGenerator();
@@ -41,54 +53,92 @@ class Toolbox extends \smtech\StMarksReflexiveCanvasLTI\Toolbox
         return $this->graphWidth() * GRAPH_ASPECT_RATIO;
     }
 
-    public function averageTurnAround($departmentId = false)
+    public function getMostCurrentCourseTimestamp($courseId)
     {
-        $stats = $this->mysql_query("
-            SELECT * FROM `course_statistics`
-                WHERE
-                    `average_grading_turn_around` > 0 " .
-                    (
-                        $departmentId ?
-                            "AND `course[account_id]` = '$departmentId' " :
-                            ''
-                    ) . "
-                GROUP BY
-                    `course[id]`
-                ORDER BY
-                    `timestamp` DESC
-        ");
-
-        $total = 0;
-        $divisor = 0;
-        while ($row = $stats->fetch_assoc()) {
-            $total += $row['average_grading_turn_around'] * $row['student_count'] * $row['graded_assignment_count'];
-            $divisor += $row['student_count'] * $row['graded_assignment_count'];
-        }
-        return $total / $divisor;
+        return $this->getCourseHistory($courseId)[0]['timestamp'];
     }
 
-    public function averageAssignmentCount($departmentId = false)
+    public function getDepartmentId($courseId)
     {
-        $stats = $this->mysql_query("
-            SELECT * FROM (
-                SELECT * FROM `course_statistics`" .
-                    (
-                        $departmentId ? "
-                            WHERE
-                                `course[account_id]` = '$departmentId'" :
-                            ''
-                    ) . "
+        return substr($this->getCourseHistory($courseId)[0]['course[account_id]'], 0, 10);
+    }
+
+    public function getCourseHistory($courseId)
+    {
+        if (empty($this->courseHistory)) {
+            if ($response = $this->mysql_query("
+                SELECT * FROM `course_statistics`
+                    WHERE
+                        `course[id]` = '$courseId'
                     ORDER BY
                         `timestamp` DESC
-            ) AS `stats`
-                GROUP BY
-                    `course[id]`
-        ");
-
-        $total = 0;
-        while ($row = $stats->fetch_assoc()) {
-            $total += $row['assignments_due_count'] + $row['dateless_assignment_count'];
+            ")) {
+                while ($row = $response->fetch_assoc()) {
+                    $this->courseHistory[] = $row;
+                }
+            }
         }
-        return $total / $stats->num_rows;
+        return $this->courseHistory;
+    }
+
+    public function getCourseSnapshot($courseId)
+    {
+        $history = $this->getCourseHistory($courseId);
+        if (!empty($history)) {
+            return $history[0];
+        }
+        return false;
+    }
+
+    public function getDepartmentSnapshot($courseId)
+    {
+        return $this->getSnapshot($courseId, self::DEPT);
+    }
+
+    public function getSchoolSnapshot($courseId)
+    {
+        return $this->getSnapshot($courseId, self::SCHOOL);
+    }
+
+    public function getSnapshot($courseId, $domain = self::DEPT)
+    {
+        if (empty($this->snapshots[$domain])) {
+            if ($response = $this->mysql_query("
+                SELECT * FROM `course_statistics`
+                    WHERE
+                        " . ($domain === self::DEPT ? "`course[account_id]` = '" . $this->getDepartmentId($courseId) . "' AND" : '') . "
+                        `timestamp` LIKE '" . $this->getMostCurrentCourseTimestamp($courseId) . "%'
+                    GROUP BY
+                        `course_id`
+                    ORDER BY
+                        `timestamp` DESC
+            ")) {
+                while ($row = $response->fetch_assoc()) {
+                    $this->schoolSnapshot[] = $row;
+
+                    /* average turn-around */
+                    $totalTurnAround += $row['average_grading_turn_around'] * $row['student_count'] * $row['graded_assignment_count'];
+                    $divisorTurnAroud += $row['student_count'] * $row['graded_assignment_count'];
+
+                    /* average assignment count */
+                    $totalAssignmentCount += $row['assignments_due_count'] + $row['dateless_assignment_count'];
+                }
+                $this->numbers[self::DEPT][self::AVERAGE_TURN_AROUND] = $totalTurnAround / $divisorTurnAround;
+                $this->numbers[self::DEPT][self::AVERAGE_ASSIGNMENT_COUNT] = $total / $response->num_rows;
+            }
+        }
+        return $this->schoolSnapshot;
+    }
+
+    public function averageTurnAround($courseId, $domain = self::DEPT)
+    {
+        $this->getSnapshot($courseId, $domain);
+        return $this->numbers[$domain][self::AVERAGE_TURN_AROUND];
+    }
+
+    public function averageAssignmentCount($courseId, $domain = self::DEPT)
+    {
+        $this->getSnapshot($courseId, $domain);
+        return $this->numbers[$domain][self::AVERAGE_ASSIGNMENT_COUNT];
     }
 }
