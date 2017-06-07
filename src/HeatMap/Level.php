88484b4baca5fdb4bf4e5eaeb346422e3fa534f5
Seth Battis <sethbattis@stmarksschool.org>
2017-06-07 16:24:43 -0400

    Documentation

--- src/HeatMap/Level.php
+++ src/HeatMap/Level.php
@@ -1,19 +1,43 @@
 <?php
+/** Level class */
 
 namespace smtech\GradingAnalytics\HeatMap;
 
+/**
+ * Comparable levels of something
+ *
+ * Used for generating heatmap-style color-coding
+ *
+ * @author Seth Battis <SethBattis@stmarksschool.org>
+ */
 class Level
 {
-    public static $GREATER_THAN = 0;
-    public static $GREATER_THAN_OR_EQUAL = 1;
-    public static $LESS_THAN_OR_EQUAL = 2;
-    public static $LESS_THAN = 3;
-
+    /**
+     * Level of color-coding (less is better)
+     * @var integer
+     */
     public $level;
+
+    /**
+     * Cut-off value for level
+     * @var integer
+     */
     public $value;
+
+    /**
+     * Comparison direction to determine inclusion in level
+     * @var Comparison
+     */
     public $comparison;
 
-    public function __construct($level, $value, $comparison)
+    /**
+     * Construct a Level object
+     *
+     * @param integer $level Color-coding level
+     * @param integer $value Cut-off value
+     * @param Comparison $comparison Comparison direction
+     */
+    public function __construct($level, $value, Comparison $comparison)
     {
         $this->level = $level;
         $this->value = $value;
--- src/Toolbox.php
+++ src/Toolbox.php
@@ -1,4 +1,5 @@
 <?php
+/** Toolbox class */
 
 namespace smtech\GradingAnalytics;
 
@@ -7,14 +8,35 @@ use smtech\GradingAnalytics\Snapshots\Domain;
 use smtech\GradingAnalytics\Snapshots\History;
 use smtech\GradingAnalytics\Snapshots\Snapshot;
 
+/**
+ * A reflexive Canvas LTI toolbox
+ *
+ * @author Seth Battis <SethBattis@stmarksschool.org>
+ */
 class Toolbox extends \smtech\StMarksReflexiveCanvasLTI\Toolbox
 {
     const TOOL_CANVAS_EXTERNAL_TOOL_ID = 'TOOL_CANVAS_EXTERNAL_TOOL_ID';
 
+    /**
+     * Course statistics history
+     * @var History
+     */
     protected $history;
 
+    /**
+     * Course statistics snapshots
+     * @var Statistic[]
+     */
     protected $snapshots;
 
+    /**
+     * Configure LTI configuration XML generator to include course and account
+     * navigation placements.
+     *
+     * {@inheritDoc}
+     *
+     * @return \smtech\LTI\Configuration\Generator
+     */
     public function getGenerator()
     {
         parent::getGenerator();
@@ -33,6 +55,13 @@ class Toolbox extends \smtech\StMarksReflexiveCanvasLTI\Toolbox
         return $this->generator;
     }
 
+    /**
+     * Extend load configuration to reset `TOOL_CANVAS_EXTERNAL_TOOL_ID`
+     *
+     * @param string $configFilePath
+     * @param  boolean $forceRecache
+     * @return void
+     */
     protected function loadConfiguration($configFilePath, $forceRecache = false)
     {
         parent::loadConfiguration($configFilePath, $forceRecache);
@@ -42,23 +71,17 @@ class Toolbox extends \smtech\StMarksReflexiveCanvasLTI\Toolbox
         }
     }
 
-    private $GRAPH_DATA_COUNT = 0;
-    public function graphWidth($dataCount = false)
-    {
-        if ($dataCount) {
-            $this->GRAPH_DATA_COUNT = $dataCount;
-        }
-        return max(GRAPH_MIN_WIDTH, $this->GRAPH_DATA_COUNT * GRAPH_BAR_WIDTH);
-    }
-
-    public function graphHeight($dataCount = false)
-    {
-        if ($dataCount) {
-            $this->GRAPH_DATA_COUNT = $dataCount;
-        }
-        return $this->graphWidth() * GRAPH_ASPECT_RATIO;
-    }
-
+    /**
+     * Get the course statistics history for a particular course
+     *
+     * @link https://smtech.github.io/grading-analytics/definitions.html Online
+     *       documentation of course statistic fields
+     *
+     * @param string|integer $courseId Numerica Canvas course ID
+     * @return array|false One row per course statistic collected daily, as
+     *                         described in the documentation, or `FALSE` if no
+     *                         statistics are available
+     */
     public function getHistory($courseId)
     {
         if (!is_a($this->history, History::class)) {
@@ -67,6 +90,30 @@ class Toolbox extends \smtech\StMarksReflexiveCanvasLTI\Toolbox
         return $this->history->getHistory();
     }
 
+    /**
+     * Get a snapshot across a particular domain relevant to a course or
+     * department
+     *
+     * @link https://smtech.github.io/grading-analytics/definitions.html Online
+     *       documentation of course statistic fields
+     *
+     * @param string|integer $courseOrDepartmentId A numeric Canvas course or
+     *        ccount ID (course is is assumed unless explicitly specified by
+     *        `$isCourseId`)
+     * @param  Domain $domain Domain across which to take snapshot
+     * @param  boolean $isCourseId (Optional, defaults to `TRUE`) whether or
+     *                             not `$courseOrDepartmentId` is a course
+     *                             (`TRUE`) or department (`FALSE`) Id
+     * @param string|integer|boolean $teacherFilter (Optional, defaults to
+     *                                              `FALSE`) An optional filter
+     *                                              to limit the snapshot data
+     *                                              to courses taught by a
+     *                                              particular numerica Canvas
+     *                                              user ID
+     * @return array|false One row per course statistic collected daily, as
+     *                         described in the documentation, or `FALSE` if no
+     *                         statistics are available
+     */
     public function getSnapshot($courseOrDepartmentId, Domain $domain, $isCourseId = true, $teacherFilter = false)
     {
         $d = $domain->getValue();
